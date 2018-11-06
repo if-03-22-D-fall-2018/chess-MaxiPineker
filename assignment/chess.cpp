b285d2849a8dfea72c019d0df8001f667985d428
Maxi Pineker <maxi.pineker@gmail.com>
2018-11-06 17:56:54 +0100

    finsihed chess.cpp

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -10,6 +10,7 @@
  * Implementation of basic chess functions.
  * ----------------------------------------------------------
  */
+void setup_chess_board(ChessBoard chess_board){
 
  //White_Pawn
  for (size_t i = 0; i < 8; i++)
@@ -25,3 +26,62 @@
    chess_board[6][i].piece.color=Black;
    chess_board[6][i].is_occupied=true;
  }
+ //White_Rook
+ chess_board[0][0].piece.type=Rook;
+ chess_board[0][0].piece.type=White;
+ chess_board[0][0].is_occupied=true;
+ chess_board[0][7].piece.type=Rook;
+ chess_board[0][7].piece.type=White;
+ chess_board[0][7].is_occupied=true;
+ //black_rook
+ chess_board[7][0].piece.type=Rook;
+ chess_board[7][0].piece.type=Black;
+ chess_board[7][0].is_occupied;
+ chess_board[7][7].piece.type=Rook;
+ chess_board[7][7].piece.type=Black;
+ chess_board[7][7].is_occupied;
+ //White_Knight
+ chess_board[0][1].piece.type=Knight;
+ chess_board[0][1].piece.color=White;
+ chess_board[0][1].is_occupied=true;
+ chess_board[0][6].piece.type=Knight;
+ chess_board[0][6].piece.color=White;
+ chess_board[0][6].is_occupied=true;
+ //Black_Knight
+ chess_board[7][1].piece.type=Knight;
+ chess_board[7][1].piece.color=White;
+ chess_board[7][1].is_occupied=true;
+ chess_board[7][6].piece.type=Knight;
+ chess_board[7][6].piece.color=White;
+ chess_board[7][6].is_occupied=true;
+ //White_Bishop
+ chess_board[0][2].piece.type=Bishop;
+ chess_board[0][2].piece.color=White;
+ chess_board[0][2].is_occupied=true;
+ chess_board[0][5].piece.type=Bishop;
+ chess_board[0][5].piece.color=White;
+ chess_board[0][5].is_occupied=true;
+ //Black_Bishop
+ chess_board[7][2].piece.type=Bishop;
+ chess_board[7][2].piece.color=Black;
+ chess_board[7][2].is_occupied=true;
+ chess_board[7][5].piece.type=Bishop;
+ chess_board[7][5].piece.color=Black;
+ chess_board[7][5].is_occupied=true;
+ //White King
+ chess_board[0][3].piece.type=King;
+ chess_board[0][3].piece.color=White;
+ chess_board[0][3].is_occupied=true;
+ //Black_King
+ chess_board[7][3].piece.type=King;
+ chess_board[7][3].piece.color=Black;
+ chess_board[7][3].is_occupied=true;
+ //White_Queen
+ chess_board[0][4].piece.type=Queen;
+ chess_board[0][4].piece.color=White;
+ chess_board[0][4].is_occupied=true;
+ //Black_Queen
+ chess_board[7][4].piece.type=Queen;
+ chess_board[7][4].piece.color=Black;
+ chess_board[7][4].is_occupied=true;
+}
