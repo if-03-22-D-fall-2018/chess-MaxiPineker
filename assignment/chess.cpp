c7ff5b8d0ac229fc7ebdcc1089ed19b612f9efbe
Maxi Pineker <maxi.pineker@gmail.com>
2018-11-06 17:41:39 +0100

    start the file chess.cpp

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: 2AHDV
+ *				HTBLA-Leonding / Klasse: 2DHIF
  * ---------------------------------------------------------
  * Exercise Number: 0
  * Title:			chess.c
@@ -10,3 +10,18 @@
  * Implementation of basic chess functions.
  * ----------------------------------------------------------
  */
+
+ //White_Pawn
+ for (size_t i = 0; i < 8; i++)
+ {
+   chess_board[1][i].piece.type=Pawn;
+   chess_board[1][i].piece.color=White;
+   chess_board[1][i].is_occupied=true;
+ }
+ //Black_Pawn
+ for (size_t i = 0; i < 8; i++)
+ {
+   chess_board[6][i].piece.type=Pawn;
+   chess_board[6][i].piece.color=Black;
+   chess_board[6][i].is_occupied=true;
+ }
