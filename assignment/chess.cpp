5a8fdd27c608a8689d0e52ea4a2e3088e778eb34
Maxi Pineker <maxi.pineker@gmail.com>
2018-11-11 22:47:11 +0100

    tried to finish the program some functions still don't work

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -12,6 +12,11 @@
  */
 #include "chess.h"
 
+
+bool are_coordinates_ok(File file1, Rank rank1, File file2, Rank rank2)
+{
+   return file1 <= 'h' && file1 >= 'a' && rank1 <= 8 && rank1 >= 1&&file2 <= 'h' && file2 >= 'a' && rank2 <= 8 && rank2 >= 1;
+}
 bool setup_chess_board(ChessBoard chess_board)
  {
   return false;
@@ -33,12 +38,11 @@ struct ChessSquare* get_square(ChessBoard chess_board,File file,Rank rank)
     return 0;
   }
   return &chess_board[rank -1][file - 'a'];
-}is_occupied
+}
 
-struct ChessPiece get_piece(ChessBoard chess_board, File a, Rank rank)is_occupied
+struct ChessPiece get_piece(ChessBoard chess_board, File file, Rank rank)
 {
-  struct ChessPiece p;
-  return p;
+  return get_square(chess_board,file,rank)->piece;
 }
 
 bool is_square_occupied(ChessBoard chess_board,File file,Rank rank)
@@ -69,19 +73,24 @@ bool is_square_occupied(ChessBoard chess_board,File file,Rank rank)
 
 bool add_piece(ChessBoard chess_board,File file,Rank rank, ChessPiece piece)
 {
-  if (!is_square_occupied(chess_board,file,rank)&& file <= 'h' && file >= 'a' && rank <= 8 && rank >= 1) {
-    chess_board[rank - 'a'][file - 1].piece = piece;
-    chess_board[rank - 'a'][file - 1].is_occupied = true;
-    return true;
-  }
-  return false;
+  if (!is_square_occupied(chess_board,file,rank)&& file <= 'h' && file >= 'a' && rank <= 8 && rank >= 1)
+{
+  chess_board[rank-1][file-'a'].piece=piece;
+ chess_board[rank-1][file-'a'].is_occupied=true;
+ return true;
+}
+return false;
 }
 
 
-bool remove_piece(ChessBoard chess_board, File file, Rank rank)
-
-{
-  return false;
+bool remove_piece(ChessBoard chess_board, File file, Rank rank){
+  if (is_square_occupied(chess_board,file,rank))
+   {
+     chess_board[rank-1][file-'a'].is_occupied = false;
+     chess_board[rank-1][file-'a'].piece.type = NoPiece;
+      return true;
+    }
+    return false;
 }
 
 
@@ -90,43 +99,56 @@ bool is_piece(struct ChessPiece piece, enum Color color, enum PieceType type)
     return (piece.color == color && piece.type == type);
 }
 
-bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+bool 	squares_share_file (File file1, Rank rank1, File file2, Rank rank2)
 {
+  if (are_coordinates_ok(file1,rank1,file2,rank2))
+ {
+   return file1==file2;
+ }
   return false;
 }
 
-
-bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
-{is_occupied
+bool 	squares_share_rank (File file1, Rank rank1, File file2, Rank rank2)
+{
+  if (are_coordinates_ok(file1,rank1,file2,rank2))
+{
+  return rank1==rank2;
+}
   return false;
 }
 
+bool 	squares_share_diagonal (File file1, Rank rank1, File file2, Rank rank2){
 
-bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
-{
   return false;
 }
 
-
-bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+bool 	squares_share_knights_move (File file1, Rank rank1, File file2, Rank rank2)
 {
-  return false;
+  return ((rank1 - 2 == rank2 || rank1 + 2 == rank2) && (file1 - 1 - 'a' == file2 - 'a' || file1 + 1 - 'a' == file2 - 'a')) || ((rank1 - 1 == rank2 || rank1 + 1 == rank2) && (file1 - 2 - 'a' == file2 - 'a' || file1 + 2 - 'a' == file2 - 'a'));
 }
 
 
 bool 	squares_share_pawns_move (enum Color color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r)
 {
-  return false;
+return false;
 }
 
 
-bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+bool 	squares_share_queens_move (File file1, Rank rank1, File file2, Rank rank2)
 {
-  return false;
+  return squares_share_diagonal(file1,rank1,file2,rank2)||squares_share_file(file1,rank1,file2,rank2)||squares_share_rank(file1,rank1,file2,rank2);
 }
 
 
-bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+bool 	squares_share_kings_move (File file1, Rank rank1, File file2, Rank rank2)
 {
-  return false;
-}
+  bool move_up= rank1+1==rank2 && file1==file2;
+  bool move_down= rank1-1==rank2 && file1==file2;
+  bool move_right= rank1==rank2 && file1+1==file2;
+  bool move_left= rank1==rank2 && file1-1==file2;
+  bool move_right_up= rank1+1==rank2 && file1+1==file2;
+  bool move_right_down= rank1-1==rank2 && file1+1==file2;
+  bool move_left_up= rank1+1==rank2 && file1-1==file2;
+  bool move_left_down= rank1-1==rank2 && file1-1==file2;
+  return move_up || move_down || move_left || move_right || move_left_up || move_right_up || move_left_down || move_right_down;
+ }
